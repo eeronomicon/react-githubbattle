a107e2db772c3094fd7010e67088da78677c4965
HK Kahng <hk@router-rooter.com>
2016-11-23 15:08:09 -0800

    add code to play with props and nested components

--- app/index.js
+++ app/index.js
@@ -4,12 +4,41 @@ var ReactDOM = require('react-dom');
 var HelloWorld = React.createClass({
   render: function() {
     return (
-      <div>Hallo Doge!</div>
+      <div>Hallo {this.props.subject}!</div>
     )
   }
 })
 
+var FriendsContainer = React.createClass({
+  render: function(){
+    var name = 'Wanda'
+    var friends = ['Daisy', 'Nate', 'Pocky']
+    return (
+      <div>
+        <div className='jumbotron'><h1> {name} </h1></div>
+        <ShowList doge={name} names={friends} />
+      </div>
+    )
+  }
+});
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li className='list-group-item'> {friend} </li>;
+    });
+    return (
+      <div>
+        <ul className='list-group'>
+          <li className='list-group-item active'><h3> Friends of {this.props.doge} </h3></li>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
 ReactDOM.render(
-  <HelloWorld />,
+  <div className='container'><FriendsContainer /></div>,
   document.getElementById('app')
 )
